887b080e706ac12b437c38a24909b039740a46a2
Rózsa Gábor <gabeszrozsa@Rozsas-MacBook-Pro.local>
2018-12-25 14:12:50 +0100

    Albums :star:

---
+++ src/components/AlbumList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+export default class AlbumList extends React.Component {
+  state = {
+      albums: []
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(res => this.setState({ albums: res.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums
+      .map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+}
